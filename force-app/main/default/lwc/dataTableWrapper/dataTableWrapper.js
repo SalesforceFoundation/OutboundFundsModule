79876e4fbae4e9abd1b44362b66e79139d8cab70
Stevie Menye M'Ella <sella@salesforce.com>
2021-06-02 16:34:45 -0500

    Use a lwc datatable

---
+++ force-app/main/default/lwc/dataTableWrapper/dataTableWrapper.js
@@ -0,0 +1,15 @@
+import { LightningElement, api } from "lwc";
+
+export default class DataTableWrapper extends LightningElement {
+    @api keyField = "id";
+    @api records = [];
+    @api columns = [];
+
+    handleRowChange(event) {
+        const draftValue = event.detail.draftValues[0];
+        const custEvent = new CustomEvent("dratfvaluechange", {
+            detail: draftValue,
+        });
+        this.dispatchEvent(custEvent);
+    }
+}
